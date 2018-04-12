012bd04ee26bfb5d09aa3c0a467d7e58949401f2
JULIO RODRIGUEZ <JULRODRI@uat.edu>
2018-04-12 13:13:50 -0700

    ll

---
+++ Drink/Drink.h
@@ -0,0 +1,7 @@
+//start of the header file 
+#pragma once
+#include <string>
+//changing the way we use to print stuff out instead of using cout 
+void print(std::string printThis);
+//Replacing the cin
+std::string hear(std::string PromptUser); 
